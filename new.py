f2d509383e6f29d8a5d4967d5e4424378f069e9a
Andy <andy7093li@gmail.com>
2022-08-12 10:32:03 -0400

    Updated Agent

--- new.py
+++ new.py
@@ -369,7 +369,7 @@ class MiRoClient:
 def turn_right(robot):
     robot.drive(0.3,-0.3)
     time.sleep(0.5)
-    robot.drive(0.2, -0.2)
+    robot.drive(0.3, -0.3)
     time.sleep(0.5)
     robot.drive(0.1, -0.1)
     time.sleep(0.5)
